e4f5cf6230de3ce671faa18c73d938a72ec5bf83
Thomas Scheffler <github@yagee.de>
2010-10-26 14:54:25 +0000

    added goal: swf-pages
    generates SWF pages
    
    git-svn-id: file:///Users/thosch/test/mycore-svn/svn/maven/datamodel-plugin/trunk@19092 7089d063-bc14-496d-ac90-53de1872eb6c

--- src/main/java/org/mycore/plugins/datamodel/AbstractDatamodelMojo.java
+++ src/main/java/org/mycore/plugins/datamodel/AbstractDatamodelMojo.java
@@ -4,10 +4,31 @@
 package org.mycore.plugins.datamodel;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.settings.Settings;
+import org.codehaus.mojo.xml.AbstractXmlMojo;
+import org.codehaus.mojo.xml.TransformMojo;
+import org.codehaus.mojo.xml.transformer.NameValuePair;
+import org.codehaus.mojo.xml.transformer.TransformationSet;
+import org.codehaus.plexus.components.io.filemappers.FileMapper;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * @author Thomas Scheffler (yagee)
@@ -17,9 +38,10 @@ public abstract class AbstractDatamodelMojo extends AbstractMojo {
 
     /**
      * Datamodel directory
-     * @parameter expression="${basedir}/src/main/datamodel"
+     * @parameter expression="${basedir}/src/main/datamodel/def"
      */
     private File dataModelDirectory;
+
     /**
      * @parameter expression="${project}"
      * @required
@@ -46,6 +68,14 @@ public abstract class AbstractDatamodelMojo extends AbstractMojo {
      * @readonly
      */
     private File basedir;
+
+    private String XSL_URI = "http://www.w3.org/1999/XSL/Transform";
+
+    /**
+     * Validate input files
+     * @parameter expression="true";
+     */
+    private boolean validate = true;
 
     /**
      * @return the dataModelDirectory
@@ -101,6 +131,68 @@ public abstract class AbstractDatamodelMojo extends AbstractMojo {
      */
     protected final void setBasedir(File basedir) {
         this.basedir = basedir;
+    }
+
+    protected TransformMojo getTransformMojo(File styleFile, File outputDirectory, File inputFileOrDir, FileMapper fm, Properties parameters)
+            throws NoSuchFieldException, IllegalAccessException {
+        TransformationSet transformationSet = new TransformationSet();
+        if (inputFileOrDir.isFile()) {
+            transformationSet.setDir(inputFileOrDir.getParentFile());
+            transformationSet.setIncludes(new String[] { inputFileOrDir.getName() });
+        } else {
+            transformationSet.setDir(inputFileOrDir);
+        }
+        transformationSet.setOutputDir(outputDirectory);
+        transformationSet.setStylesheet(styleFile);
+        transformationSet.setValidating(validate);
+        transformationSet.setFileMappers(new FileMapper[] { fm });
+        if (parameters != null) {
+            @SuppressWarnings("unchecked")
+            Entry<String, String>[] entries = parameters.entrySet().toArray(new Map.Entry[0]);
+            NameValuePair[] p = new NameValuePair[entries.length];
+            for (int i = 0; i < entries.length; i++) {
+                p[i] = new NameValuePair();
+                p[i].setName(entries[i].getKey());
+                p[i].setValue(entries[i].getValue());
+            }
+            transformationSet.setParameters(p);
+        }
+        TransformMojo transformMojo = new TransformMojo();
+        transformMojo.setLog(getLog());
+        Field transformationSets = TransformMojo.class.getDeclaredField("transformationSets");
+        transformationSets.setAccessible(true);
+        transformationSets.set(transformMojo, new TransformationSet[] { transformationSet });
+        Field project = AbstractXmlMojo.class.getDeclaredField("project");
+        project.setAccessible(true);
+        project.set(transformMojo, getProject());
+        return transformMojo;
+    }
+
+    protected void writeStylesheet(Document styleDoc, File styleFile) throws TransformerException {
+        DOMSource domSource = new DOMSource(styleDoc);
+        StreamResult streamResult = new StreamResult(styleFile);
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer serializer = tf.newTransformer();
+        serializer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        serializer.setOutputProperty(OutputKeys.INDENT, "yes");
+        serializer.transform(domSource, streamResult);
+    }
+
+    protected Document getStylesheet(String resourceFile) throws ParserConfigurationException {
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+        Element root = doc.createElementNS(XSL_URI, "stylesheet");
+        doc.appendChild(root);
+        Element include = doc.createElementNS(XSL_URI, "include");
+        include.setAttribute("href", resourceFile);
+        root.appendChild(include);
+        return doc;
+    }
+
+    protected void prepareOutputDirectory(File f) throws MojoExecutionException {
+        if (!f.exists()) {
+            if (!f.mkdirs())
+                throw new MojoExecutionException("Could not create directory" + f.getAbsolutePath());
+        }
     }
 
 }
--- src/main/java/org/mycore/plugins/datamodel/GenerateSchema.java
+++ src/main/java/org/mycore/plugins/datamodel/GenerateSchema.java
@@ -1,24 +1,12 @@
 package org.mycore.plugins.datamodel;
 
 import java.io.File;
-import java.lang.reflect.Field;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.apache.maven.plugin.MojoExecutionException;
-import org.codehaus.mojo.xml.AbstractXmlMojo;
 import org.codehaus.mojo.xml.TransformMojo;
-import org.codehaus.mojo.xml.transformer.TransformationSet;
 import org.codehaus.plexus.components.io.filemappers.FileMapper;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 /**
  * Goal that generates XML Schema files from MyCoRe datamodel 2 files.
@@ -28,32 +16,24 @@ import org.w3c.dom.Element;
  * @phase generate-resources
  */
 public class GenerateSchema extends AbstractDatamodelMojo {
-    private String XSL_URI = "http://www.w3.org/1999/XSL/Transform";
-
     /**
      * Location of the file.
      * @parameter expression="${project.build.outputDirectory}"
      * @required
      */
-    private File outputDirectory;
-
-    /**
-     * Validate input files
-     * @parameter expression="true";
-     */
-    private boolean validate = true;
+    private File schemaDirectory;
 
     public void execute() throws MojoExecutionException {
-        File f = outputDirectory;
-
-        if (!f.exists()) {
-            if (!f.mkdirs())
-                throw new MojoExecutionException("Could not create directory" + f.getAbsolutePath());
-        }
+        prepareOutputDirectory(getSchemaDirectory());
         try {
             Document styleDoc = getStylesheet("datamodel2schema.xsl");
-            File styleFile = writeStylesheet(styleDoc);
-            TransformMojo transformMojo = getTransformMojo(styleFile);
+            File styleFile = new File(schemaDirectory, this.getClass().getCanonicalName() + ".xsl");
+            writeStylesheet(styleDoc, styleFile);
+            TransformMojo transformMojo = getTransformMojo(styleFile, getSchemaDirectory(), getDataModelDirectory(), new FileMapper() {
+                public String getMappedFileName(String fileName) {
+                    return "datamodel-" + fileName.substring(0, fileName.length() - 4) + ".xsd";
+                }
+            }, null);
             transformMojo.execute();
             if (!styleFile.delete())
                 throw new MojoExecutionException("Could not delete temporary file: " + styleFile.getAbsolutePath());
@@ -67,60 +47,14 @@ public class GenerateSchema extends AbstractDatamodelMojo {
     /**
      * @return the outputDirectory
      */
-    protected final File getOutputDirectory() {
-        return outputDirectory;
+    protected final File getSchemaDirectory() {
+        return schemaDirectory;
     }
 
     /**
-     * @param outputDirectory the outputDirectory to set
+     * @param schemaDirectory the outputDirectory to set
      */
-    protected final void setOutputDirectory(File outputDirectory) {
-        this.outputDirectory = outputDirectory;
-    }
-
-    private TransformMojo getTransformMojo(File styleFile) throws NoSuchFieldException, IllegalAccessException {
-        TransformationSet transformationSet = new TransformationSet();
-        transformationSet.setDir(getDataModelDirectory());
-        transformationSet.setOutputDir(outputDirectory);
-        transformationSet.setStylesheet(styleFile);
-        transformationSet.setValidating(validate);
-        FileMapper fm = new FileMapper() {
-            public String getMappedFileName(String fileName) {
-                return "datamodel-" + fileName.substring(0, fileName.length() - 4) + ".xsd";
-            }
-        };
-        transformationSet.setFileMappers(new FileMapper[] { fm });
-        TransformMojo transformMojo = new TransformMojo();
-        transformMojo.setLog(getLog());
-        Field transformationSets = TransformMojo.class.getDeclaredField("transformationSets");
-        transformationSets.setAccessible(true);
-        transformationSets.set(transformMojo, new TransformationSet[] { transformationSet });
-        Field project = AbstractXmlMojo.class.getDeclaredField("project");
-        project.setAccessible(true);
-        getLog().info("Project: " + getProject());
-        project.set(transformMojo, getProject());
-        return transformMojo;
-    }
-
-    private File writeStylesheet(Document styleDoc) throws TransformerException {
-        DOMSource domSource = new DOMSource(styleDoc);
-        File styleFile = new File(outputDirectory, this.getClass().getCanonicalName() + ".xsl");
-        StreamResult streamResult = new StreamResult(styleFile);
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer serializer = tf.newTransformer();
-        serializer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        serializer.setOutputProperty(OutputKeys.INDENT, "yes");
-        serializer.transform(domSource, streamResult);
-        return styleFile;
-    }
-
-    private Document getStylesheet(String resourceFile) throws ParserConfigurationException {
-        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-        Element root = doc.createElementNS(XSL_URI, "stylesheet");
-        doc.appendChild(root);
-        Element include = doc.createElementNS(XSL_URI, "include");
-        include.setAttribute("href", resourceFile);
-        root.appendChild(include);
-        return doc;
+    protected final void setSchemaDirectory(File schemaDirectory) {
+        this.schemaDirectory = schemaDirectory;
     }
 }
